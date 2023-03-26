e5b56d84741df0db0b305ae2bd3931d8a70771ba
viethoang <viethoang12112002@gmail.com>
2023-03-26 14:02:58 +0700

    bailu star rail

--- test/echoclient.c
+++ test/echoclient.c
@@ -1,3 +1,63 @@
+// #include "syscall.h"
+// // #include <stdio.h>
+// // #include <stdlib.h>
+// // #include <string.h>
+// // #include <sys/socket.h>
+// // #include <netinet/in.h>
+// // #include <arpa/inet.h>
+// // #include <unistd.h>
+
+// #define SERVER_ADDR "127.0.0.1"
+// #define SERVER_PORT 5000
+// #define NUM_SOCKETS 1
+
+// int main(){
+//     int sockets[NUM_SOCKETS];
+//     char message[] = "Hello from socket viet";
+//     char* buffer= message;
+//     char* server_addr="127.0.0.1";
+//     int i=0;
+//     int port=5000;
+//     for(i=0;i<NUM_SOCKETS;i++){
+//         sockets[i]= SocketTCP2();
+//          if (sockets[i] < 0) {
+//             Write("socket creation failed: \n",string_length,1);
+//             return -1;
+//         }
+//     }
+//     for (i = 0; i < NUM_SOCKETS; i++) {
+//         if (Connect(sockets[i],  server_addr, port) < 0) {
+//             Write("socket connection failed: \n",string_length,1);
+//             return -1;
+//         }
+    
+//     }
+    
+//     for (i = 0; i < NUM_SOCKETS; i++) {
+//         // sprintf(buffer, "%s%d\n", message, i);
+//         if (Send(sockets[i], message, 255) < 0) {
+//             Write("socket send failed: \n",string_length,1);
+//         }
+//         // CloseSocketTCP(sockets[i]);
+//         // printf("Socket %d closed\n", i);
+//     }
+//     for (i=0; i< NUM_SOCKETS; i++){
+//         char reply[1024];
+//         if (Receive(sockets[i], reply, 1024) < 0) {
+//         Write("socket recv failed: \n",string_length,1);
+//         return -1;
+//     }
+//         Write("Socket capitalized: \n",30,1);
+//         Write(reply,string_length,1);
+//         if (CloseSocketTCP(sockets[i]) <0 )
+//         Write("socket closed: \n",string_length,1);
+//     }
+
+//     // Write("tem2",string_length,1);
+//     return 0;
+// }
+
+
 #include "syscall.h"
 // #include <stdio.h>
 // #include <stdlib.h>
@@ -9,15 +69,16 @@
 
 #define SERVER_ADDR "127.0.0.1"
 #define SERVER_PORT 5000
-#define NUM_SOCKETS 1
+#define NUM_SOCKETS 4
 
 int main(){
     int sockets[NUM_SOCKETS];
-    char message[] = "Hello from socket viet";
+    char message[] = "Bin laden";
     char* buffer= message;
     char* server_addr="127.0.0.1";
     int i=0;
     int port=5000;
+    char* reply1,reply2,reply3,reply4;
     for(i=0;i<NUM_SOCKETS;i++){
         sockets[i]= SocketTCP2();
          if (sockets[i] < 0) {
@@ -41,18 +102,23 @@ int main(){
         // CloseSocketTCP(sockets[i]);
         // printf("Socket %d closed\n", i);
     }
+    Write("Messages capitalized:\n",string_length,1);
     for (i=0; i< NUM_SOCKETS; i++){
         char reply[1024];
         if (Receive(sockets[i], reply, 1024) < 0) {
         Write("socket recv failed: \n",string_length,1);
         return -1;
     }
-        Write("Socket capitalized: \n",30,1);
+        Write(" \n",string_length,1);
         Write(reply,string_length,1);
+        
         if (CloseSocketTCP(sockets[i]) <0 )
-        Write("socket closed: \n",string_length,1);
+        Write("socket closed failed: \n",string_length,1);
+        else{
+            
+            Write("\nsocket closed sucessfully: \n",string_length,1);}
     }
 
-    // Write("tem2",string_length,1);
+    // Write("tem2 \n",string_length,1);
     return 0;
 }
