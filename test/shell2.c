13f26c46938877c1cf213955ae33d9dbb9db9e48
viethoang1211 <viethoang12112002@gmail.com>
2023-05-03 11:35:00 +0700

    Update shell2.c

--- test/shell2.c
+++ test/shell2.c
@@ -5,16 +5,8 @@ int
 main()
 {
     int tem1;
-    int tem2;
     SpaceId newProc1;
-    SpaceId newProc2;
-
     newProc1 = Exec("cat"); // Project 01
-    newProc2 = Exec("copy"); // Project 01
-
     tem1= Join(newProc1);
-    tem2= Join(newProc2);
     if (tem1!=0) PrintNum(tem1);
-    if (tem2!=0) PrintNum(tem2);
-
 }
